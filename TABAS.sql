c6c4035154596e26c23688c07a6f87f934265660
Nickolas Rodriguez <ce.nickolasrc@gmail.com>
2019-06-01 10:44:03 -0600

    adicion de una posible tabla y otras mas

--- TABAS.sql
+++ TABAS.sql
@@ -1,79 +1,93 @@
+-- insert listo
 CREATE TABLE Employee (
   FirstName varchar (15),
   LastName varchar (15),
-  ID_number integer PRIMARY KEY,
+  ID_number integer PRIMARY KEY NOT NULL,
   Role varchar
 );
 
+-- insert listo
 CREATE TABLE Client (
+  ClientID integer PRIMARY KEY NOT NULL,
   FirstName varchar (15),
   LastName varchar (15),
-  Email varchar (320) PRIMARY KEY,
+  Email varchar (320),
   Phone integer,
   Carnet varchar (30)
 );
 
-CREATE TABLE Suitcase (
+-- insert listo
+CREATE TABLE SuitCase (
+  SuitCaseID integer PRIMARY KEY NOT NULL,
   Color varchar (10),
   Weight integer,
   Cost integer,
-  SuitcaseID integer PRIMARY KEY,
   State BOOLEAN
 );
 
+-- insert listo
 CREATE TABLE BagCart (
-  BagCartID integer PRIMARY KEY,
+  BagCartID integer PRIMARY KEY NOT NULL,
   Brand varchar (30),
   Model integer,
   Sec_Code varchar (10)
 );
 
+-- insert listo
 CREATE TABLE Flight (
+  FlightID integer PRIMARY KEY NOT NULL,
   Departure_Date timestamp,
   Origin varchar (30),
-  FlightID integer PRIMARY KEY,
   Destiny varchar
 );
 
+-- insert listo
 CREATE TABLE Plane (
-  PlaneID integer PRIMARY KEY
+  PlaneID integer PRIMARY KEY NOT NULL
 );
 
+-- insert listo
 CREATE TABLE PlaneModel (
-  PlaneModelID integer PRIMARY KEY,
+  PlaneModelID integer PRIMARY KEY NOT NULL,
   CellarNumber integer
 );
 
 CREATE TABLE Cellar (
-  CellarID integer PRIMARY KEY,
+  CellarID integer PRIMARY KEY NOT NULL,
   Capacity integer
 );
 
-CREATE TABLE ClientXSuitcase (
-  ClientEmail varchar (320),
-  SuitcaseID integer,
-  PRIMARY KEY (ClientEmail, SuitcaseID)
+-- insert listo
+CREATE TABLE ClientXSuitCase (
+  ClientID integer,
+  SuitCaseID integer,
+  PRIMARY KEY (ClientEmail, SuitCaseID)
 );
 
+-- insert listo
 CREATE TABLE SuitCaseXBagCart (
-  SuitcaseID integer,
+  SuitCaseID integer,
   BagCartID integer,
-  PRIMARY KEY (SuitcaseID, BagCartID)
+  PRIMARY KEY (SuitCaseID, BagCartID)
 );
 
+-- insert listo
 CREATE TABLE FlightXBagCart (
   BagCartID integer,
   FlightID integer,
   PRIMARY KEY (BagCartID, FlightID)
 );
 
+
+-- insert listo
 CREATE TABLE PlaneXFlight (
   FlightID integer,
   PlaneID integer,
   PRIMARY KEY (FlightID, PlaneID)
 );
 
-CREATE TABLE PlaneXPlaneModel (
+-- insert listo
+CREATE TABLE  (
   PlaneID integer,
   PlaneModelID integer,
   PRIMARY KEY (PlaneID, PlaneModelID)
@@ -85,11 +99,17 @@ CREATE TABLE PlaneXCellar (
   PRIMARY KEY (PlaneID, CellarID)
 );
 
-ALTER TABLE ClientXSuitcase ADD FOREIGN KEY (ClientEmail) REFERENCES Client (Email);
-ALTER TABLE ClientXSuitcase ADD FOREIGN KEY (SuitcaseID) REFERENCES Suitcase (SuitcaseID);
+CREATE TABLE SuitCaseXCellar (
+  SuitCaseID INTEGER,
+  CellarID INTEGER,
+  PRIMARY KEY (SuitCaseID, CellarID)
+)
+
+ALTER TABLE ClientXSuitCase ADD FOREIGN KEY (ClientID) REFERENCES Client (ClientID);
+ALTER TABLE ClientXSuitCase ADD FOREIGN KEY (SuitCaseID) REFERENCES SuitCase (SuitCaseID);
 
 ALTER TABLE SuitCaseXBagCart ADD FOREIGN KEY (BagCartID) REFERENCES BagCart (BagCartID);
-ALTER TABLE SuitCaseXBagCart ADD FOREIGN KEY (SuitcaseID) REFERENCES Suitcase (SuitcaseID);
+ALTER TABLE SuitCaseXBagCart ADD FOREIGN KEY (SuitCaseID) REFERENCES SuitCase (SuitCaseID);
 
 ALTER TABLE FlightXBagCart ADD FOREIGN KEY (FlightID) REFERENCES Flight (FlightID);
 ALTER TABLE FlightXBagCart ADD FOREIGN KEY (BagCartID) REFERENCES BagCart (BagCartID);
@@ -102,3 +122,6 @@ ALTER TABLE PlaneXPlaneModel ADD FOREIGN KEY (PlaneID) REFERENCES Plane (PlaneID
 
 ALTER TABLE PlaneXCellar ADD FOREIGN KEY (PlaneID) REFERENCES Plane (PlaneID);
 ALTER TABLE PlaneXCellar ADD FOREIGN KEY (CellarID) REFERENCES Cellar (CellarID);
+
+ALTER TABLE SuitCaseXCellar ADD FOREIGN KEY (SuitCaseID) REFERENCES SuitCase (SuitCaseID);
+ALTER TABLE SuitCaseXCellar ADD FOREIGN KEY (CellarID) REFERENCES Cellar (CellarID);
