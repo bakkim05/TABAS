7b7afe5efaaae36faddc248d4bb55ac7c0bf87e7
Jung <bakkim05@gmail.com>
2019-06-01 22:30:59 -0600

    +Routing for pages in vistaWeb

--- tabas/src/app/app-routing.module.ts
+++ tabas/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page404Component } from './page404/page404.component';
+import { MaletasComponent } from './maletas/maletas.component';
+import { BagCartsComponent } from './bag-carts/bag-carts.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
+import { CreateMaletasComponent } from './create-maletas/create-maletas.component';
+import { CreateBagCartsComponent } from './create-bag-carts/create-bag-carts.component';
+import { CreateClientesComponent } from './create-clientes/create-clientes.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'login'           ,    component: LogInComponent},
+  {path: 'register'        ,    component: RegisterComponent},
+  {path: 'maletas'         ,    component: MaletasComponent},
+  {path: 'bagCarts'        ,    component: BagCartsComponent},
+  {path: 'clientes'        ,    component: ClientesComponent},
+  {path: 'createMaletas'   ,    component: CreateMaletasComponent},
+  {path: 'createBagCarts'  ,    component: CreateBagCartsComponent},
+  {path: 'createClientes'  ,    component: CreateClientesComponent},
+  {path: '**'              ,    component: Page404Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
