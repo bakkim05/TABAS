d9ad0b3e696398789c5168d25f1de5afd30e18e8
Jung <bakkim05@gmail.com>
2019-06-05 21:16:08 -0600

    editar implementado para clientes, bagCarts y maletas

--- tabas/src/app/edit-bag-carts/edit-bag-carts.component.ts
+++ tabas/src/app/edit-bag-carts/edit-bag-carts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-edit-bag-carts',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-bag-carts.component.css']
 })
 export class EditBagCartsComponent implements OnInit {
+  
+  private id = new FormControl('');
+  private marca = new FormControl('');
+  private modelo = new FormControl('');
+  private sec_code = new FormControl('');
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  createJSON(){
+    var sent : any = {};
+
+    sent.code = "editClientes";
+
+    sent.bagCart = {};
+    sent.bagCart.id = parseInt(this.id.value);
+    sent.bagCart.marca = this.marca.value;
+    sent.bagCart.modelo = this.modelo.value;
+    sent.bagCart.sec_code = parseInt(this.sec_code.value);
+
+    return sent;
+  }
+
+  sendPost(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers' : 'Content-Type',
+        'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS'
+      })
+    };
+
+    this.http.post("http://httpbin.org/post",this.createJSON(),httpOptions)
+      .toPromise()
+      .then(data => {
+        console.log(data);
+      });
+
+    // this.router.navigate(['clientes']);
   }
 
 }
