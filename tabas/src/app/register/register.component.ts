d209474e61f320b9ffd05adad3b30b0d0b64810f
Jung <bakkim05@gmail.com>
2019-06-05 22:19:53 -0600

    Register User implemented

--- tabas/src/app/register/register.component.ts
+++ tabas/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  private nombre = new FormControl('');
+  private apellido = new FormControl('');
+  private rol = new FormControl('');
+  private username = new FormControl('');
+  private password = new FormControl('');
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  createJSON(){
+    var sent : any = {};
+
+    sent.code = "registrarUsuario";
+
+    sent.usuario = {};
+    sent.usuario.nombre = this.nombre.value;
+    sent.usuario.apellido = this.apellido.value;
+    sent.usuario.rol = parseInt(this.rol.value);
+    sent.usuario.username = this.username.value;
+    sent.usuario.password = this.password.value;
+
+    return sent;
+  }
+
+  sendPost(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers' : 'Content-Type',
+        'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS'
+      })
+    };
+
+    this.http.post("http://httpbin.org/post",this.createJSON(),httpOptions)
+      .toPromise()
+      .then(data => {
+        console.log(data);
+      });
+
+    // this.router.navigate(['clientes']);
   }
 
 }
