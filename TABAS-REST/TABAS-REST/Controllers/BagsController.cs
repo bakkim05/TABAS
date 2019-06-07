291f3bccbefeb0d7de506538d1a0614fec26009e
Dagoberto Keylor Rojas Juarez <krojas96@estudiantec.cr>
2019-06-07 14:30:29 -0600

    adición de clases de administracion de maletas, empleados y coches

---
+++ TABAS-REST/TABAS-REST/Controllers/BagsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TABAS_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BagsController : ControllerBase
+    {
+        // GET: api/Bags
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Bags/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Bags
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Bags/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
