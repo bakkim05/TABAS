2930db5244d1d0dae8e3da6e18719a2cdea668ae
unknown <Administrador@LAIMI2-B26.estudiantes.tec.cr>
2019-06-07 22:56:56 -0600

    edicion de vista de maletas

--- tabas/src/app/maletas/maletas.component.ts
+++ tabas/src/app/maletas/maletas.component.ts
@@ -12,7 +12,7 @@ export class MaletasComponent implements OnInit {
   private MALETAS_DATA = []
   private maletasObservable : Observable<any[]>;
 
-  displayedColumns: string[] = ['id','color','weight','cost','state','correo'];
+  displayedColumns: string[] = ['id','color','weight','cost','state','cedula'];
   dataSource = new MatTableDataSource(this.MALETAS_DATA);
 
   applyFilter (filterValue: string){
